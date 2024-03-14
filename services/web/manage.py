d0af7cf6073d0108cd4302a3390dc3be75034f4c
Fredmont <alexfredmont@yandex.ru>
2024-03-14 19:42:22 +0500

    edit flow

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,6 +1,6 @@
 from flask.cli import FlaskGroup
 
-from project import app, db, User
+from project import app, db, Ticket
 
 
 cli = FlaskGroup(app)
@@ -15,7 +15,13 @@ def create_db():
 
 @cli.command("seed_db")
 def seed_db():
-    db.session.add(User(email="michael@mherman.org"))
+    #db.session.add(Ticket(email="michael@mherman.org"))
+    db.session.add(Ticket(ticket_id="76986496: 16391710",
+                          order_id="1234",
+                          status_raw="ok",
+                          event_id=1,
+                          date="2024-01-01T10:39:32+00:00",
+                          organization_id=123))
     db.session.commit()
 
 
--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,56 +1,68 @@
 import os
-
+import logging
 from flask import (
     Flask,
     jsonify,
     send_from_directory,
     request,
+    make_response
 )
+
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
-
+from datetime import datetime
+import pytz 
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
+logging.basicConfig(filename='request_log.txt', level=logging.DEBUG)
 
-
-class User(db.Model):
-    __tablename__ = "users"
-
+class Ticket(db.Model):
+    __tablename__ = "tickets"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+    ticket_id = db.Column(db.String(128), nullable=False, unique=True)
+    organization_id = db.Column(db.Integer)
+    order_id = db.Column(db.String(128), nullable=False)
+    status_raw = db.Column(db.String(128), nullable=False)
+    event_id = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime(timezone=True), nullable=False)
 
-    def __init__(self, email):
-        self.email = email
+    def __init__(self, 
+                 ticket_id, 
+                 order_id,
+                 status_raw,
+                 event_id,
+                 date,
+                 organization_id=0
+                 ):
+        self.ticket_id = ticket_id
+        self.order_id = order_id
+        self.status_raw = status_raw
+        self.event_id = event_id
+        self.date = date
+        self.organization_id = organization_id
 
-
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
-
-
-@app.route("/static/<path:filename>")
-def staticfiles(filename):
-    return send_from_directory(app.config["STATIC_FOLDER"], filename)
-
-
-@app.route("/media/<path:filename>")
-def mediafiles(filename):
-    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
-
-
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        file = request.files["file"]
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
-    return """
-    <!doctype html>
-    <title>upload new File</title>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file><input type=submit value=Upload>
-    </form>
-    """
+@app.route('/log', methods=['GET', 'POST'])
+def log_request():
+    # Log the details of the incoming request
+    data = request.get_json()
+    app.logger.info('Request received: %s %s. Data: %s', request.method, request.path, data.dump())
+    str_timestamp = data['hook_generated_at']
+    # Parse the string into a datetime object with time zone information
+    dt = pytz.timezone("UTC").localize(datetime.strptime(str_timestamp, "%Y-%m-%d %H: %M: %S"))
+    dataset = db.session.execute(db.select(Ticket).filter_by(ticket_id = data['id'])).scalars()
+    dataset_len = len(dataset.all())
+    app.logger.info(dataset_len)
+    if dataset_len == 0:
+        db.session.add(Ticket(ticket_id=data['id'],
+                            order_id=data['order_id'],
+                            status_raw=data['status_raw'],
+                            event_id=data['event_id'],
+                            date=dt,
+                            organization_id=data['organization_id']))
+        db.session.commit()
+        
+        return make_response('Logged', 200)
+    else:
+        return make_response('Not logged', 200)
